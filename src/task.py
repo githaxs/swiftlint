82ec24cb564feddfedd70525e361de050163a55b
Gabe Levasseur <11285350+GabeL7r@users.noreply.github.com>
2022-06-04 18:26:05 -0400

    feat: implement swiftlint

--- src/task.py
+++ src/task.py
@@ -1,30 +1,12 @@
-from task_interfaces import TaskInterface, SubscriptionLevels, TaskTypes
+import os
+from task_interfaces import StaticAnalysisTask, SubscriptionLevels
 
 
-class Task(TaskInterface):
+class Task(StaticAnalysisTask):
     """
     A tool to enforce Swift style and conventions.
     """
 
     name = "Swift Lint"
-    slug = "swift-lint "
-    subscription_level = SubscriptionLevels.FREE
-    actions = None
-
-    # For code format tasks, specify if they can automatically fix errors.
-    # If they can there will be a button for users to click in github ui.
-    can_fix = False
-
-    # Valid types include:
-    # CODE_FORMAT: clones the repo and runs shell scripts against the contents
-    # CODE_ANALYSIS: not used
-    # WORKFLOW: only works with meta data from the webhook, i.e. repo not cloned
-    # DEPLOY: still experimental - runs in ECS
-    # UNIT_TEST: still experimental - runs in ECS
-    type = TaskTypes.FOO
+    subscription_level = SubscriptionLevels.STARTUP
     source_script_path = "%s/task.sh" % os.path.dirname(__file__)
-    handler = "task"
-
-    # Use the execute function if the task type is WORKFLOW
-    def execute(self, github_body, settings) -> bool:
-        pass
